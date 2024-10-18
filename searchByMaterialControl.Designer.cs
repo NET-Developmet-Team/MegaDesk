b0944544e8d7aca76e634b7c833e2ca2c941a1b9
kyfrostbyte <94023660+kyfrostbyte@users.noreply.github.com>
2024-10-17 20:02:23 -0600

    Adjusted control form layout

--- searchByMaterialControl.Designer.cs
+++ searchByMaterialControl.Designer.cs
@@ -41,10 +41,10 @@
             // nameLabel
             // 
             this.nameLabel.AutoSize = true;
-            this.nameLabel.Font = new System.Drawing.Font("Garamond", 18F, System.Drawing.FontStyle.Bold);
+            this.nameLabel.Font = new System.Drawing.Font("Garamond", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.nameLabel.Location = new System.Drawing.Point(13, 14);
             this.nameLabel.Name = "nameLabel";
-            this.nameLabel.Size = new System.Drawing.Size(93, 34);
+            this.nameLabel.Size = new System.Drawing.Size(91, 33);
             this.nameLabel.TabIndex = 0;
             this.nameLabel.Text = "Name";
             this.nameLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -52,90 +52,89 @@
             // dateLabel
             // 
             this.dateLabel.Font = new System.Drawing.Font("Garamond", 18F, System.Drawing.FontStyle.Bold);
-            this.dateLabel.Location = new System.Drawing.Point(467, 14);
+            this.dateLabel.Location = new System.Drawing.Point(576, 14);
             this.dateLabel.Name = "dateLabel";
-            this.dateLabel.Size = new System.Drawing.Size(273, 31);
+            this.dateLabel.Size = new System.Drawing.Size(159, 31);
             this.dateLabel.TabIndex = 1;
             this.dateLabel.Text = "Date";
-            this.dateLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.dateLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // widthLabel
             // 
             this.widthLabel.AutoSize = true;
             this.widthLabel.Font = new System.Drawing.Font("Garamond", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.widthLabel.Location = new System.Drawing.Point(166, 69);
+            this.widthLabel.Location = new System.Drawing.Point(23, 73);
             this.widthLabel.Margin = new System.Windows.Forms.Padding(0);
             this.widthLabel.Name = "widthLabel";
-            this.widthLabel.Size = new System.Drawing.Size(78, 30);
+            this.widthLabel.Size = new System.Drawing.Size(65, 25);
             this.widthLabel.TabIndex = 3;
             this.widthLabel.Text = "width";
-            this.widthLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.widthLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // depthLabel
             // 
             this.depthLabel.AutoSize = true;
             this.depthLabel.Font = new System.Drawing.Font("Garamond", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.depthLabel.Location = new System.Drawing.Point(165, 128);
+            this.depthLabel.Location = new System.Drawing.Point(23, 132);
             this.depthLabel.Margin = new System.Windows.Forms.Padding(0, 0, 0, 3);
             this.depthLabel.Name = "depthLabel";
-            this.depthLabel.Size = new System.Drawing.Size(79, 30);
+            this.depthLabel.Size = new System.Drawing.Size(65, 25);
             this.depthLabel.TabIndex = 5;
             this.depthLabel.Text = "depth";
-            this.depthLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.depthLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // areaLabel
             // 
             this.areaLabel.AutoSize = true;
             this.areaLabel.Font = new System.Drawing.Font("Garamond", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.areaLabel.Location = new System.Drawing.Point(436, 67);
+            this.areaLabel.Location = new System.Drawing.Point(314, 73);
             this.areaLabel.Margin = new System.Windows.Forms.Padding(0);
             this.areaLabel.Name = "areaLabel";
-            this.areaLabel.Size = new System.Drawing.Size(61, 30);
+            this.areaLabel.Size = new System.Drawing.Size(52, 25);
             this.areaLabel.TabIndex = 6;
             this.areaLabel.Text = "area";
-            this.areaLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.areaLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // drawerLabel
             // 
             this.drawerLabel.AutoSize = true;
             this.drawerLabel.Font = new System.Drawing.Font("Garamond", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.drawerLabel.Location = new System.Drawing.Point(436, 127);
+            this.drawerLabel.Location = new System.Drawing.Point(313, 133);
             this.drawerLabel.Margin = new System.Windows.Forms.Padding(0, 0, 0, 3);
             this.drawerLabel.Name = "drawerLabel";
-            this.drawerLabel.Size = new System.Drawing.Size(102, 30);
+            this.drawerLabel.Size = new System.Drawing.Size(86, 25);
             this.drawerLabel.TabIndex = 11;
             this.drawerLabel.Text = "drawers";
-            this.drawerLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.drawerLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // materialLabel
             // 
             this.materialLabel.AutoSize = true;
             this.materialLabel.Font = new System.Drawing.Font("Garamond", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.materialLabel.Location = new System.Drawing.Point(615, 63);
+            this.materialLabel.Location = new System.Drawing.Point(576, 73);
             this.materialLabel.Margin = new System.Windows.Forms.Padding(0);
             this.materialLabel.Name = "materialLabel";
-            this.materialLabel.Size = new System.Drawing.Size(107, 30);
+            this.materialLabel.Size = new System.Drawing.Size(90, 25);
             this.materialLabel.TabIndex = 12;
             this.materialLabel.Text = "material";
-            this.materialLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.materialLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
             // priceLabel
             // 
             this.priceLabel.AutoSize = true;
             this.priceLabel.Font = new System.Drawing.Font("Garamond", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.priceLabel.Location = new System.Drawing.Point(657, 121);
+            this.priceLabel.Location = new System.Drawing.Point(576, 132);
             this.priceLabel.Margin = new System.Windows.Forms.Padding(0, 0, 0, 3);
             this.priceLabel.MinimumSize = new System.Drawing.Size(50, 0);
             this.priceLabel.Name = "priceLabel";
-            this.priceLabel.Size = new System.Drawing.Size(71, 30);
+            this.priceLabel.Size = new System.Drawing.Size(58, 25);
             this.priceLabel.TabIndex = 13;
             this.priceLabel.Text = "price";
-            this.priceLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.priceLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
             // searchByMaterialControl
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
-            this.AutoSize = true;
             this.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.Controls.Add(this.priceLabel);
             this.Controls.Add(this.materialLabel);
